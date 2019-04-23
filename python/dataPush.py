5b197acc93ad4d21058f84c7458727ceb0d00aa3
Parker Siroishka <parkersir10@hotmail.com>
2019-04-23 14:25:44 -0600

    Removed weighted point equivalents from database.
    Only includes actual statistics now

--- python/dataPush.py
+++ python/dataPush.py
@@ -6,19 +6,12 @@ import itertools
 firebase = firebase.FirebaseApplication('https://statgen-993f4.firebaseio.com/')
 
 kills = 4
-killsMult = 1
 errors = 6
-errorsMult = -1
 assists = 9
-assistsMult = 0.25
 aces = 11
-acesMult = 2
 digs = 13
-digsMult = 1
 blocksSolo = 15
-blocksSoloMult = 2
 blocksAss = 16
-blocksAssMult = 1
 
 #Each teams hashtable
 calgaryRoster = hashData("calgaryData.csv")
@@ -47,17 +40,15 @@ def calcStatsList(schoolRoster, playerName):
 	else:
 		playerPosition = (schoolRoster[playerName][1])
 
-	killPoints = (schoolRoster[playerName][kills]) * killsMult 
-	assistPoints = (schoolRoster[playerName][assists]) * assistsMult 
-	acesPoints = (schoolRoster[playerName][aces]) * acesMult 
-	digPoints = (schoolRoster[playerName][digs]) * digsMult
-	blocksSoloPoints = (schoolRoster[playerName][blocksSolo]) * blocksSoloMult
-	blocksAssPoints = (schoolRoster[playerName][blocksAss]) * blocksAssMult
-	errorPoints = (schoolRoster[playerName][errors]) * errorsMult 
+	killsTOT = (schoolRoster[playerName][kills])
+	assistsTOT = (schoolRoster[playerName][assists])
+	acesTOT = (schoolRoster[playerName][aces])
+	digsTOT = (schoolRoster[playerName][digs])
+	blocksSoloTOT = (schoolRoster[playerName][blocksSolo])
+	blocksAssistsTOT = (schoolRoster[playerName][blocksAss])
+	errorsTOT = (schoolRoster[playerName][errors])
 
-	totalPoints = killPoints+assistPoints+acesPoints+digPoints+blocksSoloPoints+blocksAssPoints+errorPoints
-
-	allStats = [totalPoints, killPoints, assistPoints, acesPoints, digPoints, blocksSoloPoints, blocksAssPoints, errorPoints, playerPosition]
+	allStats = [killsTOT, assistsTOT, acesTOT, digsTOT, blocksSoloTOT, blocksAssistsTOT, errorsTOT, playerPosition]
 
 	return  allStats
 
@@ -68,7 +59,10 @@ def putData():
 		for player in roster:
 
 			try:
-				result =firebase.put(('/playerData/'+school), player, {'position':str(calcStatsList(roster, player)[8]),'total points':str(calcStatsList(roster, player)[0]),'kill points':str(calcStatsList(roster, player)[1]),'assist points':str(calcStatsList(roster, player)[2]),'ace points':str(calcStatsList(roster, player)[3]), 'dig points': str(calcStatsList(roster, player)[4]), 'blocks solo points': str(calcStatsList(roster, player)[5]), 'blocks assists points': str(calcStatsList(roster, player)[6]), 'error points': str(calcStatsList(roster, player)[7])})
+				playerStats = calcStatsList(roster, player)
+				print(playerStats)
+				result =firebase.put(('/playerData/'+school), player, {'position':str(playerStats[7]),'killsTOT':str(playerStats[0]),'assistsTOT':str(playerStats[1]),'acesTOT':str(playerStats[2]),\
+																	   'digsTOT': str(playerStats[3]), 'blocks soloTOT': str(playerStats[4]), 'blocks assistsTOT': str(playerStats[5]), 'errorsTOT': str(playerStats[6])})
 			except TypeError:
 				pass
 
